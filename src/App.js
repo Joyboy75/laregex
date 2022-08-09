3b34ca6963daa672cd3e20512f1421970fabc927
OO <joyboy75>
2022-08-09 02:47:57 +0200

    Components 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.css'
+import Header from './Components/header/Header'
+import Nav from './Components/nav/Nav'
+import About from './Components/about/About'
+import Experience from './Components/experience/Experience'
+import Services from './Components/services/Services'
+import Portfolio from './Components/portfolio/Portfolio'
+import Testimonials from './Components/testimonials/Testimonials'
+import Contact from './Components/contact/Contact'
+import Footer from './Components/footer/Footer'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
