16fc03171d3c2d6a89448114f5c6433a216239e3
OO <joyboy75>
2022-08-25 13:53:34 +0200

    Footer and CSS

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import './footer.css'
+import { FaFacebook } from 'react-icons/fa'
+import { IoLogoTwitter } from 'react-icons/io'
+import { FiInstagram } from 'react-icons/fi'
 
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <a href="" className="footer__logo">
+        EGATOR
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact ">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook">
+          <FaFacebook />
+        </a>
+        <a href="https://instagram">
+          <IoLogoTwitter />
+        </a>
+        <a href="https://twitter.com">
+          {' '}
+          <FiInstagram />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy;Oumar Diouara tous droits réservés</small>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
